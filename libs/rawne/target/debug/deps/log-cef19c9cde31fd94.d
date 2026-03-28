79c9cab5d7ff2a91edbd48ac7480b84e5080e6bd
PH <phg@live.com>
2026-03-28 11:36:32 +0800

    fix: 消除 rawne 预留常量死代码警告 [skip ci]

---
+++ libs/rawne/target/debug/deps/log-cef19c9cde31fd94.d
@@ -0,0 +1,10 @@
+d:\RustDesk_New\libs\rawne\target\debug\deps\log-cef19c9cde31fd94.d: C:\Users\Administrator\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\lib.rs C:\Users\Administrator\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\macros.rs C:\Users\Administrator\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\serde.rs C:\Users\Administrator\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\__private_api.rs
+
+d:\RustDesk_New\libs\rawne\target\debug\deps\liblog-cef19c9cde31fd94.rlib: C:\Users\Administrator\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\lib.rs C:\Users\Administrator\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\macros.rs C:\Users\Administrator\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\serde.rs C:\Users\Administrator\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\__private_api.rs
+
+d:\RustDesk_New\libs\rawne\target\debug\deps\liblog-cef19c9cde31fd94.rmeta: C:\Users\Administrator\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\lib.rs C:\Users\Administrator\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\macros.rs C:\Users\Administrator\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\serde.rs C:\Users\Administrator\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\__private_api.rs
+
+C:\Users\Administrator\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\lib.rs:
+C:\Users\Administrator\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\macros.rs:
+C:\Users\Administrator\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\serde.rs:
+C:\Users\Administrator\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\__private_api.rs:
---
+++ libs/rawne/target/debug/deps/rawne-ecb0fa5401b07fb6.d
@@ -0,0 +1,15 @@
+d:\RustDesk_New\libs\rawne\target\debug\deps\rawne-ecb0fa5401b07fb6.d: src\lib.rs src\sade.rs src\zhcc.rs src\efec.rs src\capture.rs src\route.rs src\svd.rs src\sdwan.rs src\common.rs
+
+d:\RustDesk_New\libs\rawne\target\debug\deps\librawne-ecb0fa5401b07fb6.rlib: src\lib.rs src\sade.rs src\zhcc.rs src\efec.rs src\capture.rs src\route.rs src\svd.rs src\sdwan.rs src\common.rs
+
+d:\RustDesk_New\libs\rawne\target\debug\deps\librawne-ecb0fa5401b07fb6.rmeta: src\lib.rs src\sade.rs src\zhcc.rs src\efec.rs src\capture.rs src\route.rs src\svd.rs src\sdwan.rs src\common.rs
+
+src\lib.rs:
+src\sade.rs:
+src\zhcc.rs:
+src\efec.rs:
+src\capture.rs:
+src\route.rs:
+src\svd.rs:
+src\sdwan.rs:
+src\common.rs:
